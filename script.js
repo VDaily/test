c7f97f00e4a76e0ddaac894ac130639bd850d38d
VDaily <workingEmail2207@gmail.com>
2022-12-19 15:58:02 +0300

    made change [master]

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ class OneClass {
     console.log("fix critical error");
   }
 }
+class TwoClass {
+  constructor() {
+  }
 
+  method1() {
+    return "333";
+  }
+}
 const objArr = [
   { id: 0, title: "Book1"},
   { id: 1, title: "Book Mr.Supper"}
